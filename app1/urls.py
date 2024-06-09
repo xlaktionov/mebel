c4d7e4a278ad65ae6f57e7d18621b34caa941032
PavelProject <example@gmail.com>
2024-06-09 23:40:22 +0300

    The main app is ready

--- app1/urls.py
+++ app1/urls.py
@@ -15,13 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from app.main import views
+from django.urls import path, include
+
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
-    path('about/', views.about, name='about'),
+    path('', include('main.urls', namespace='main'))
+
 ]
--- main/views.py
+++ main/views.py
@@ -5,8 +5,17 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request) -> HttpResponse:
-    return render(request, 'main/index.html')
+    context = {
+        'title': 'Home - Главная',
+        'content': 'Магазин мебели HOME'
+    }
+    return render(request, 'main/index.html', context)
 
 
 def about(request) -> HttpResponse:
-    return HttpResponse('Home page')
+    context = {
+        'title': 'Home - о нас',
+        'content': 'О нас',
+        'text_on_page': 'Текст о самом магазине (шаблон)'
+    }
+    return render(request, 'main/about.html', context)
