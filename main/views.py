8801054c5869844803b981a301410697a2619ad5
PavelProject <example@gmail.com>
2024-06-24 00:34:05 +0300

    slug converter product

--- main/views.py
+++ main/views.py
@@ -8,12 +8,10 @@ from goods.models import Categories
 
 def index(request) -> HttpResponse:
 
-    categories = Categories.objects.all()
 
     context = {
         'title': 'Home - Главная',
         'content': 'Магазин мебели HOME',
-        'categories': categories,
     }
     return render(request, 'main/index.html', context)
 
